1311d3177a6c867133801f46454e1a0935c5bc9d
Etxau24 <96jonetxauri@gmail.com>
2020-05-01 12:44:34 +0200

    Creado ficheros de probabilidad (.c y .h)

--- persona.h
+++ persona.h
@@ -30,10 +30,3 @@
  // DECISION DE MUERTE DE UNA PERSONA
  // (Par: struct persona)
  int matarPersona(Persona *per);
-
- // CALCULAR UNA EDAD ENTRE 0 y 100
- // (Par: int edad media de la poblacion)
- int numeroRandom(int medEdad) ;
-
- // CALCULAR NUMERO ENTRE 0 y 1 (DECISION DE MUERTE)
- float calcProb();
