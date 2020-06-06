54383b6f1a523bfcae4bd446d086e7270f509b85
Etxau24 <96jonetxauri@gmail.com>
2020-06-06 20:22:34 +0200

    :((

--- main.c
+++ main.c
@@ -228,7 +228,7 @@ int main(int argc, char** argv) {
 		}
 
 		printf("STATUS: MUERTEEEEEEEEEEEEEEEEE\n");
-
+		struct almacenamiento *holi;
 		// PASAR DEL LINKEDLIST A ARRAY
 		for(e=0; e<4; e++){
 			envios[e].capacidad=cap[e].capacidad;
@@ -236,7 +236,8 @@ int main(int argc, char** argv) {
 			for(i=0;i<envios[e].capacidad;i++){
 				envios[e].personas[i]=cap[e].actualPersona;
 				printf("STATUS: ESTAMOS AQUI HAH 2, W.R=%d, cap[%d]=%p\n", world_rank, i, cap[e]);
-				cap[e]=&cap[e].siguienteAlma;
+				holi=cap[e].siguienteAlma;
+				//cap[e]=*cap[e].siguienteAlma;
 				printf("STATUS: ESTAMOS AQUI HAH 3, W.R=%d, cap[%d]=%p\n", world_rank, i, cap[e]);
 			}
 		}
