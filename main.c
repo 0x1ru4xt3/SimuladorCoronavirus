9d2d18993aa205f788a2b1a2e0b6907e8aeef553
Etxau24 <96jonetxauri@gmail.com>
2020-06-06 20:18:20 +0200

    que miro

--- main.c
+++ main.c
@@ -235,9 +235,9 @@ int main(int argc, char** argv) {
 			envios[e].personas=malloc(envios[e].capacidad*sizeof(struct persona));
 			for(i=0;i<envios[e].capacidad;i++){
 				envios[e].personas[i]=cap[e].actualPersona;
-				printf("STATUS: ESTAMOS AQUI HAH 2\n");
+				printf("STATUS: ESTAMOS AQUI HAH 2, W.R=%d, cap[%d]=%p\n", world_rank, i, cap[e]);
 				cap[e]=*cap[e].siguienteAlma;
-				printf("STATUS: ESTAMOS AQUI HAH 3\n");
+				printf("STATUS: ESTAMOS AQUI HAH 3, W.R=%d, cap[%d]=%p\n", world_rank, i, cap[e]);
 			}
 		}
 
